f8a9cc9c972701dd3d862f8f1f42e937d8014de0
Mike <mike@unknown00155D014000.gateway.pace.com>
2016-02-02 05:39:56 -0800

    Remove ATTY compile option.
    It can't be enabled in the configure, so it is probably rarely used

--- src/var.h
+++ src/var.h
@@ -78,12 +78,7 @@ struct localvar_list;
 extern struct localvar *localvars;
 extern struct var varinit[];
 
-#if ATTY
-#define vatty varinit[0]
-#define vifs varinit[1]
-#else
 #define vifs varinit[0]
-#endif
 #define vmail (&vifs)[1]
 #define vmpath (&vmail)[1]
 #define vpath (&vmpath)[1]
@@ -136,9 +131,6 @@ extern char linenovar[];
 #define termval()	(vterm.text + 5)
 #endif
 
-#if ATTY
-#define attyset()	((vatty.flags & VUNSET) == 0)
-#endif
 // #define mpathset()	((vmpath.flags & VUNSET) == 0)
 
 struct localvar_list {
